b10bbca3e6118bd8b9a2e9bc6e177b41fc91afe2
dawnvince <dawnvince666@gmail.com>
2023-09-07 14:29:00 +0000

    modify plot

--- Analysis/Plot.py
+++ Analysis/Plot.py
@@ -5,6 +5,7 @@ font1 = {'size':10}
 font2 = {'size':12}
 label_thres = 0.5  
 dpi = 2000
+clip_rate = 0.998
     
 def plot_uts_score_only(curve, score, label, save_path):
     assert len(label) >= len(score), "Score length is longer than label length."
@@ -13,14 +14,21 @@ def plot_uts_score_only(curve, score, label, save_path):
     curve_len = len(curve)
     x = [i for i in range(curve_len)]
     
-    plt.figure(figsize=(32, 2))
-    fig1 = plt.subplot()
+    top_y = score[score.argsort()[int(clip_rate * len(score)) - 1]] * 3
+    bottom_y = score.min() - 0.1 * (top_y - score.min())
+    
+    plt.figure(figsize=(48, 4))
+    fig1 = plt.subplot(2, 1, 1)
     plt.plot(x, curve, label="raw curve", linewidth=0.1, color="red")
+    
+    plt.xticks([])
+    
     plt.legend(loc="upper left",prop=font1, handlelength=1, borderpad=0.1, handletextpad=0.3, ncol=2, columnspacing=0.5, borderaxespad=0.1)
     
-    fig2 = fig1.twinx()
+    fig2 = plt.subplot(2, 1, 2)
     plt.plot(x, score, label="anomaly score", linewidth=0.1, color="steelblue")
-    plt.legend(loc="upper right",prop=font1, handlelength=1, borderpad=0.1, handletextpad=0.3, ncol=2, columnspacing=0.5, borderaxespad=0.1)
+    plt.ylim(bottom_y, top_y)
+    plt.legend(loc="upper left",prop=font1, handlelength=1, borderpad=0.1, handletextpad=0.3, ncol=2, columnspacing=0.5, borderaxespad=0.1)
     
     # count anomaly segment
     ano_seg = []
@@ -41,10 +49,11 @@ def plot_uts_score_only(curve, score, label, save_path):
     
     for seg in ano_seg:
         fig1.axvspan(seg[0], seg[1], alpha=1, color='pink')
+        fig2.axvspan(seg[0], seg[1], alpha=1, color='pink')
     
-    plt.title("Raw Data",loc = "left", fontdict=font2)
-    plt.title("Anomaly Score",loc = "right", fontdict=font2)
-    plt.savefig("{}.pdf".format(save_path), dpi=dpi, format="pdf")
+    # plt.title("Raw Data",loc = "left", fontdict=font2)
+    # plt.title("Anomaly Score",loc = "right", fontdict=font2)
+    plt.savefig("{}.pdf".format(save_path), format="pdf")
     
     
 def plot_uts_score_and_yhat(curve, y_hat, score, label, save_path):
@@ -57,15 +66,22 @@ def plot_uts_score_and_yhat(curve, y_hat, score, label, save_path):
     curve_len = len(curve)
     x = [i for i in range(curve_len)]
     
-    plt.figure(figsize=(16, 2))
-    fig1 = plt.subplot()
+    top_y = score[score.argsort()[int(clip_rate * len(score)) - 1]] * 3
+    bottom_y = score.min() - 0.1 * (top_y - score.min())
+    
+    plt.figure(figsize=(48, 4))
+    fig1 = plt.subplot(2, 1, 1)
     plt.plot(x, curve, label="raw curve", linewidth=0.1, color="red")
     plt.plot(x, y_hat, label="y_hat", linewidth=0.1, color="green")
+    
+    plt.xticks([])
+    
     plt.legend(loc="upper left",prop=font1, handlelength=1, borderpad=0.1, handletextpad=0.3, ncol=2, columnspacing=0.5, borderaxespad=0.1)
     
-    fig2 = fig1.twinx()
+    fig2 = plt.subplot(2, 1, 2)
     plt.plot(x, score, label="anomaly score", linewidth=0.1, color="steelblue")
-    plt.legend(loc="upper right",prop=font1, handlelength=1, borderpad=0.1, handletextpad=0.3, ncol=2, columnspacing=0.5, borderaxespad=0.1)
+    plt.ylim(bottom_y, top_y)
+    plt.legend(loc="upper left",prop=font1, handlelength=1, borderpad=0.1, handletextpad=0.3, ncol=2, columnspacing=0.5, borderaxespad=0.1)
     
     # count anomaly segment
     ano_seg = []
@@ -87,7 +103,8 @@ def plot_uts_score_and_yhat(curve, y_hat, score, label, save_path):
     
     for seg in ano_seg:
         fig1.axvspan(seg[0], seg[1], alpha=1, color='pink')
+        fig2.axvspan(seg[0], seg[1], alpha=1, color='pink')
     
-    plt.title("Raw Data",loc = "left", fontdict=font2)
-    plt.title("Anomaly Score",loc = "right", fontdict=font2)
-    plt.savefig("{}.pdf".format(save_path), dpi=dpi, format="pdf")
+    # plt.title("Raw Data",loc = "left", fontdict=font2)
+    # plt.title("Anomaly Score",loc = "right", fontdict=font2)
+    plt.savefig("{}.pdf".format(save_path), format="pdf")
