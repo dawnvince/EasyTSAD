3f7720305a6b6e7ac290b966b9d790a359ddcfb7
issaccv <17275050+issaccv@users.noreply.github.com>
2024-01-22 07:46:53 +0800

    Add TranAD module and dataset classes

---
+++ EasyTSAD/Methods/TranAD/TSDataset.py
@@ -0,0 +1,87 @@
+import torch
+import torch.utils.data
+import numpy as np
+from ...DataFactory import TSData
+from typing import Tuple, Sequence, Dict
+
+
+class OneByOneDataset(torch.utils.data.Dataset):
+    """
+    The Dateset for one by one training and testing.
+    """
+
+    def __init__(self, data: np.ndarray, window_size: int) -> None:
+        super().__init__()
+        self.window_size = window_size
+
+        (self.len,) = data.shape
+
+        self.sample_num = max(self.len - self.window_size + 1, 0)
+        self.samples, self.targets = self.__getsamples(data)
+
+    def __getsamples(self, data):
+        X = torch.zeros((self.sample_num, self.window_size))
+
+        for i in range(self.sample_num):
+            X[i, :] = torch.from_numpy(data[i : i + self.window_size])
+
+        return X, X
+
+    def __len__(self):
+        return self.sample_num
+
+    def __getitem__(self, index):
+        return self.samples[index, :], self.targets[index, :]
+
+
+class AllInOneDataset(torch.utils.data.Dataset):
+    """
+    The Dateset for one by one training and testing.
+    """
+
+    def __init__(self, datas: Dict[str, TSData], phase: str, window_size: int) -> None:
+        super().__init__()
+        self.window_size = window_size
+
+        self.sample_num = 0
+
+        if phase == "train":
+            for _, i in datas.items():
+                (ll,) = i.train.shape
+                self.sample_num += max(ll - self.window_size + 1, 0)
+
+            X = torch.zeros((self.sample_num, self.window_size))
+            cnt = 0
+            for _, i in datas.items():
+                (ll,) = i.train.shape
+                ll = max(ll - self.window_size + 1, 0)
+                for j in range(ll):
+                    X[cnt, :] = torch.from_numpy(i.train[j : j + self.window_size])
+                    cnt += 1
+
+        elif phase == "valid":
+            for _, i in datas.items():
+                (ll,) = i.valid.shape
+                self.sample_num += max(ll - self.window_size + 1, 0)
+
+            X = torch.zeros((self.sample_num, self.window_size))
+            cnt = 0
+            for _, i in datas.items():
+                (ll,) = i.valid.shape
+                ll = max(ll - self.window_size + 1, 0)
+                for j in range(ll):
+                    X[cnt, :] = torch.from_numpy(i.valid[j : j + self.window_size])
+                    cnt += 1
+
+        else:
+            raise NotImplementedError
+
+        assert cnt == self.sample_num
+
+        self.samples, self.targets = X, X
+
+    def __len__(self):
+        return self.sample_num
+
+    def __getitem__(self, index):
+        return self.samples[index, :], self.targets[index, :]
--- EasyTSAD/Methods/TranAD/model.py
+++ EasyTSAD/Methods/TranAD/model.py
@@ -12,15 +12,19 @@ class PositionalEncoding(nn.Module):
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model).float() * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(
+            torch.arange(0, d_model).float() * (-math.log(10000.0) / d_model)
+        )
         pe += torch.sin(position * div_term)
         pe += torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+        self.register_buffer("pe", pe)
 
     def forward(self, x, pos=0):
-        x = x + self.pe[pos:pos+x.size(0), :]
+        # breakpoint()
+        x = x + self.pe[pos : pos + x.size(0), :]
         return self.dropout(x)
+
 
 class TransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=16, dropout=0):
@@ -40,6 +44,7 @@ class TransformerEncoderLayer(nn.Module):
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
         src = src + self.dropout2(src2)
         return src
+
 
 class TransformerDecoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=16, dropout=0):
@@ -64,36 +69,43 @@ class TransformerDecoderLayer(nn.Module):
         tgt = tgt + self.dropout3(tgt2)
         return tgt
 
+
 class TranADModel(nn.Module):
-	def __init__(self, feats):
-		super(TranADModel, self).__init__()
-		self.name = 'TranAD'
-		self.batch = 128
-		self.n_feats = feats
-		self.n_window = 10
-		self.n = self.n_feats * self.n_window
-		self.pos_encoder = PositionalEncoding(2 * feats, 0.1, self.n_window)
-		encoder_layers = TransformerEncoderLayer(d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1)
-		self.transformer_encoder = TransformerEncoder(encoder_layers, 1)
-		decoder_layers1 = TransformerDecoderLayer(d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1)
-		self.transformer_decoder1 = TransformerDecoder(decoder_layers1, 1)
-		decoder_layers2 = TransformerDecoderLayer(d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1)
-		self.transformer_decoder2 = TransformerDecoder(decoder_layers2, 1)
-		self.fcn = nn.Sequential(nn.Linear(2 * feats, feats), nn.Sigmoid())
+    def __init__(self, feats):
+        super(TranADModel, self).__init__()
+        self.name = "TranAD"
+        self.batch = 128
+        self.n_feats = feats
+        self.n_window = 10
+        self.n = self.n_feats * self.n_window
+        self.pos_encoder = PositionalEncoding(2 * feats, 0.1, self.n_window)
+        encoder_layers = TransformerEncoderLayer(
+            d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1
+        )
+        self.transformer_encoder = TransformerEncoder(encoder_layers, 1)
+        decoder_layers1 = TransformerDecoderLayer(
+            d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1
+        )
+        self.transformer_decoder1 = TransformerDecoder(decoder_layers1, 1)
+        decoder_layers2 = TransformerDecoderLayer(
+            d_model=2 * feats, nhead=feats, dim_feedforward=16, dropout=0.1
+        )
+        self.transformer_decoder2 = TransformerDecoder(decoder_layers2, 1)
+        self.fcn = nn.Sequential(nn.Linear(2 * feats, feats), nn.Sigmoid())
 
-	def encode(self, src, c, tgt):
-		src = torch.cat((src, c), dim=2)
-		src = src * math.sqrt(self.n_feats)
-		src = self.pos_encoder(src)
-		memory = self.transformer_encoder(src)
-		tgt = tgt.repeat(1, 1, 2)
-		return tgt, memory
+    def encode(self, src, c, tgt):
+        src = torch.cat((src, c), dim=2)
+        src = src * math.sqrt(self.n_feats)
+        src = self.pos_encoder(src)
+        memory = self.transformer_encoder(src)
+        tgt = tgt.repeat(1, 1, 2)
+        return tgt, memory
 
-	def forward(self, src, tgt):
-		# Phase 1 - Without anomaly scores
-		c = torch.zeros_like(src)
-		x1 = self.fcn(self.transformer_decoder1(*self.encode(src, c, tgt)))
-		# Phase 2 - With anomaly scores
-		c = (x1 - src) ** 2
-		x2 = self.fcn(self.transformer_decoder2(*self.encode(src, c, tgt)))
-		return x1, x2
+    def forward(self, src, tgt):
+        # Phase 1 - Without anomaly scores
+        c = torch.zeros_like(src)
+        x1 = self.fcn(self.transformer_decoder1(*self.encode(src, c, tgt)))
+        # Phase 2 - With anomaly scores
+        c = (x1 - src) ** 2
+        x2 = self.fcn(self.transformer_decoder2(*self.encode(src, c, tgt)))
+        return x1, x2
